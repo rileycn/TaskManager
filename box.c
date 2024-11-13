f68faed44c72c0f55b639e4284e54bf4f7662323
rileycn <rileycnoggle@gmail.com>
2024-11-13 18:46:02 -0500

    Added panels

---
+++ box.c
@@ -0,0 +1,49 @@
+#include <gtk/gtk.h>
+
+#include "box.h"
+
+GtkWidget* get_main_box() {
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    //gtk_widget_set_halign(box, GTK_ALIGN_CENTER);
+    //gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
+    gtk_widget_set_hexpand(box, TRUE);
+    gtk_widget_set_vexpand(box, TRUE);
+
+    GtkWidget *notebook = gtk_notebook_new();
+    gtk_widget_set_hexpand(notebook, TRUE);
+    gtk_widget_set_vexpand(notebook, TRUE);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), get_system_panel(), gtk_label_new("System"));
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), get_processes_panel(), gtk_label_new("Processes"));
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), get_resources_panel(), gtk_label_new("Resources"));
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), get_file_systems_panel(), gtk_label_new("File Systems"));
+
+    gtk_box_append(GTK_BOX(box), notebook);
+    
+
+    return box;
+}
+
+GtkWidget* get_system_panel() {
+    GtkWidget *tab = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget *title_label = gtk_label_new("System Content Page");
+    gtk_widget_set_margin_top(title_label, 20);
+    gtk_widget_set_margin_bottom(title_label, 20);
+    gtk_box_append(GTK_BOX(tab), title_label);
+    gtk_box_append(GTK_BOX(tab),  gtk_label_new("System info"));
+    return tab;
+}
+
+GtkWidget* get_processes_panel() {
+    GtkWidget *tab_content = gtk_label_new("(Processes Content Page)");
+    return tab_content;
+}
+
+GtkWidget* get_resources_panel() {
+    GtkWidget *tab_content = gtk_label_new("(Resources Content Page)");
+    return tab_content;
+}
+
+GtkWidget* get_file_systems_panel() {
+    GtkWidget *tab_content = gtk_label_new("(File Systems Content Page)");
+    return tab_content;
+}
--- main.c
+++ main.c
@@ -1,28 +1,24 @@
 #include <gtk/gtk.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
-{
-  GtkWidget *window;
+#include "box.c"
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_window_present (GTK_WINDOW (window));
+static void activate (GtkApplication* app, gpointer user_data) {
+  GtkWidget* window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Task");
+  gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
+
+  GtkWidget* box = get_main_box();
+
+  gtk_window_set_child(GTK_WINDOW(window), box);
+
+  gtk_window_present(GTK_WINDOW(window));
 }
 
-int
-main (int    argc,
-      char **argv)
-{
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+int main (int argc, char **argv) {
+  GtkApplication *app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
 
   return status;
 }
