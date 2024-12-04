dd5f53162f1d8c0af4998d2afd134d4dcc83e250
Riley Christian Noggle <noggler@data.cs.purdue.edu>
2024-12-04 11:00:09 -0500

    search bars in gtk are terrible

--- src/file_systems_panel.h
+++ src/file_systems_panel.h
@@ -7,7 +7,14 @@
 #define MAX_MOUNTS_SIZE (512)
 #define MOUNTS_FILE_PATH ("/proc/mounts")
 
+typedef struct {
+    GtkTreeModel *list_store;
+    GtkSearchEntry *search_entry;
+} search_data;
+
 GtkWidget* get_file_systems_panel();
 void add_to_file_system_tree(GtkListStore *store, char* device, char* dir, char* type);
+gboolean filter_func(GtkTreeModel *model, GtkTreeIter *iter, gpointer data);
+void on_search_changed(GtkSearchEntry *entry, GtkTreeModelFilter *filter);
 
 #endif
